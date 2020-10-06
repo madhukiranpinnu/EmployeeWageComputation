97f362a606576de2764f5950b4ccdeb1618e4e3f
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 15:31:01 +0530

    added parttime in a case

--- employeeComputation.sh
+++ employeeComputation.sh
@@ -25,3 +25,24 @@ then
 else
         salary=0;
 fi
+
+##checked for parttime
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ]
+then
+        empHrs=8;
+
+elif [ $isPartTime -eq $randomCheck ]
+then
+        empHrs=4;
+
+else
+        empHrs=0;
+fi
+
+salary=$(($empHrs*$empRatePerHr));
+
