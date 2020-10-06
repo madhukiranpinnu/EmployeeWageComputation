5106bf0b185f3761e0212ab3f0ea70ef1f624c38
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 15:40:17 +0530

    added monthlywages

--- employeeComputation.sh
+++ employeeComputation.sh
@@ -65,3 +65,28 @@ case $empCheck in
 esac
 
 salary=$(($empHrs*$empRatePerHr))
+ 
+## caluculating monthly wage
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+totalSalary=0
+numWorkingDays=30
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+        empCheck=$((RANDOM%3));
+        case $empCheck in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        salary=$(($empHrs*$empRatePerHr));
+        totalSalary=$(($totalSalary+$salary));
+done
