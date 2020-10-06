d95635b09bf8f40ba74c80ca74b7d66c77d1184a
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 15:35:46 +0530

    added employee file using case

--- employeeComputation.sh
+++ employeeComputation.sh
@@ -46,3 +46,22 @@ fi
 
 salary=$(($empHrs*$empRatePerHr));
 
+## employee wage in case
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+        $isFullTime)
+                empHrs=8
+                ;;
+        $isPartTime)
+                empHrs=4
+                ;;
+        *)
+                empHrs=0
+                ;;
+esac
+
+salary=$(($empHrs*$empRatePerHr))
