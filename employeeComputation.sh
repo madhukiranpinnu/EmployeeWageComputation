51fc92dddd9c2e41725d0df38e525ad896f6488c
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 15:44:42 +0530

    added working of people in a month

--- employeeComputation.sh
+++ employeeComputation.sh
@@ -90,3 +90,33 @@ do
         salary=$(($empHrs*$empRatePerHr));
         totalSalary=$(($totalSalary+$salary));
 done
+
+##employee wage in all Working hours
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        totalWorkingDays=$(($totalWorkingDays+1))
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
+done
+
