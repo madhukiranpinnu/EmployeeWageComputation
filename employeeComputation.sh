314b4abaa225631223c78e9716c97b113f3c2416
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 15:58:44 +0530

    added  employee dictionary

--- employeeComputation.sh
+++ employeeComputation.sh
@@ -198,3 +198,46 @@ totalSalary="$( calDailyWages $totalEmpHrs )"
 echo "daily wages : " ${empDailyWages[@]}
 
 
+##in dictionary employeewage
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+function getWorkingHours() {
+
+         case $1 in
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
+function calDailyWages() {
+        totalWorkHrs=$1;
+        wages=$(( $totalWorkHrs*$empRatePerHr ))
+        echo $wages;
+}
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        totalWorkingDays=$(($totalWorkingDays+1))
+        empHrs="$( getWorkingHours $((RANDOM%3)) )"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        empDailyWages[$totalWorkingDays]="$( calDailyWages $empHrs )"
+done
+
+totalSalary="$( calDailyWages $totalEmpHrs )"
+echo "daily wages : " ${empDailyWages[@]}
+echo "All Keys : " ${!empDailyWages[@]}
