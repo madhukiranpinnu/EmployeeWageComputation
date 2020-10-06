8c7288e219e478f5c972022eef4c48bf7097be53
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 15:22:06 +0530

    addded attendance of employee through branch

--- employeeComputation.sh
+++ employeeComputation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "welcome to employee wage computation "
+
+##employee attendance to check whether present or absent
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Abscent";
+fi
